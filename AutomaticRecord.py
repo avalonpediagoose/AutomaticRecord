7e1f9000bb9bce6675d90ed444f5cc5778002b19
avalonpediagoose <151800889+avalonpediagoose@users.noreply.github.com>
2024-05-30 16:25:44 +0800

    Update AutomaticRecord.py
    Version: 0.04

--- AutomaticRecord.py
+++ AutomaticRecord.py
@@ -1,139 +1,93 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 15 15:57:54 2024
+Created on Fri May 17 15:49:34 2024
 
 @author: ginachen
 """
 
-import wx
+import AP_GUI
 import datetime
-import time
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
+import wx
+#---------------------------------------------------------------------------------------------------
+""" Global variables """
 now = datetime.datetime.now()
-timestamp = now.strftime('_%m%d_%H%M%S')
-path = timestamp+'_AutoRecord.txt'
 data = now.strftime('%m%d')
 titleStr = data+' 一般場'
-
-class MyFrame(wx.Frame):
-    def __init__(self, *args, **kw):
-        super(MyFrame, self).__init__(*args, **kw, size=(438, 300))
+####################################################################################################
+""" Class Definition """ 
+class APFrame(AP_GUI.MyFrame):    
+    #""" Frame Event """----------------------------------------------------------------------------            
+    def __init__(self,title):       
+        AP_GUI.MyFrame.__init__(self, title)
         
-        # 創建面板
-        panel = wx.Panel(self)
+        APFrame.SettingINI(self)
+        print('AutomaticRecord.exe Loaded.')
+            
+    def OnClose(self, event):
+        self.Destroy()
+        app.ExitMainLoop() # Exit the main event loop
         
-        # 創建垂直佈局管理器
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        
-        # 添加變數1的文本框
-        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-        st1 = wx.StaticText(panel, label="輸入:")
-        hbox1.Add(st1, flag=wx.RIGHT, border=8)
-        self.var1 = wx.StaticText(panel, label="請輸入總局數、湖中(ex:0湖4好，則打4o)、刺殺誰(沒有請按空白鍵)")
-        hbox1.Add(self.var1, proportion=1)
-        vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
-        # 添加變數2的文本框
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        st2 = wx.StaticText(panel, label="範例1 (3局3藍刺殺):")
-        hbox2.Add(st2, flag=wx.RIGHT, border=8)
-        self.var2 = wx.StaticText(panel, label="34o5")
-        hbox2.Add(self.var2, flag=wx.RIGHT, border=8)
-        st3 = wx.StaticText(panel, label=", 範例2 (5局3紅):")
-        hbox2.Add(st3, flag=wx.RIGHT, border=8)
-        self.var3 = wx.StaticText(panel, label="54o8o1x ")
-        hbox2.Add(self.var3, proportion=1)
-        vbox.Add(hbox2, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
-        # 添加變數3的文本框
-        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-        st4 = wx.StaticText(panel, label="實戰:")
-        hbox3.Add(st4, flag=wx.RIGHT, border=8)
-        self.record_input = wx.TextCtrl(panel, value="56o2x9o6")
-        hbox3.Add(self.record_input, proportion=1)
-        vbox.Add(hbox3, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
-        # 添加變數4的文本框
-        hbox4 = wx.BoxSizer(wx.HORIZONTAL)
-        st5 = wx.StaticText(panel, label="延遲:")
-        hbox4.Add(st5, flag=wx.RIGHT, border=8)
-        self.SelRoomTime = wx.TextCtrl(panel, value="5")
-        hbox4.Add(self.SelRoomTime, proportion=1)
-        vbox.Add(hbox4, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        st6 = wx.StaticText(panel, label="秒(s)")
-        hbox4.Add(st6, proportion=1)
-        vbox.Add(hbox4, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        self.WaitSel = wx.StaticText(panel, label="")
-        self.WaitSel.SetForegroundColour(wx.Colour(255, 0, 0))
-        hbox4.Add(self.WaitSel, proportion=1)
-        vbox.Add(hbox4, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
-        # 添加記錄按鈕
-        hbox5 = wx.BoxSizer(wx.HORIZONTAL)
-        st5 = wx.StaticText(panel, label="紀錄者ID:")
-        hbox5.Add(st5, flag=wx.RIGHT, border=8)
-        self.record_output = wx.TextCtrl(panel, size=(200, -1), value="派票紀錄員")  # 設置文本框初始大小
-        hbox5.Add(self.record_output, proportion=1, flag=wx.RIGHT, border=8)
-        self.btnRecord = wx.Button(panel, label="記錄")
-        hbox5.Add(self.btnRecord, flag=wx.ALIGN_CENTER)
-        vbox.Add(hbox5, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP|wx.BOTTOM, border=10)
-        self.btnRecord.Bind(wx.EVT_BUTTON, self.on_submit)
-        
-        # 設置面板佈局
-        panel.SetSizer(vbox)
-        
-        # 設置框架屬性
-        self.SetTitle(titleStr)
-        self.Centre()
-
-    def on_submit(self, event):
+    def RecordEvent(self, event):       
         self.btnRecord.Enable(False)
         
-        ll = self.record_input.GetValue()
-        lake_lady=[i for i in ll]
-        r=int(lake_lady[0])
+        nowRecord = datetime.datetime.now()
+        timestamp = nowRecord.strftime('_%m%d_%H%M%S')
+        path = timestamp+'_AutoRecord.txt'
+        
+        ll = self.textc_ActualCombat.GetValue()
+        lake_lady = [i for i in ll]
+        r = int(lake_lady[0])
+        
         #開始跑網頁
-        options=webdriver.ChromeOptions()
+        options = webdriver.ChromeOptions()
         options.add_experimental_option('detach', True)
-        driver=webdriver.Chrome(options=options)
-        ult="https://avalon.signage-cloud.org/" #可改
+        driver = webdriver.Chrome(options = options)
+        ult = "https://avalon.signage-cloud.org/" #可改
         driver.get(ult)
         username=driver.find_element(By.CLASS_NAME,"form-control")
-        username.send_keys("派票紀錄員") #輸入玩家名字
+        TempName = self.tctrl_RecordName.GetValue()
+        username.send_keys(TempName) #輸入玩家名字
         btn=driver.find_element(By.CLASS_NAME,"btn-block") #定義button
         btn.click() #點擊進入頁面
+        self.stext_WaitSel.SetForegroundColour((255,0,0))
         print("請自行點擊需要的房間")
-        self.WaitSel.SetLabel("請自行點擊需要的房間")
-        time.sleep(int(self.SelRoomTime.GetValue()))
+        self.stext_WaitSel.SetLabel("請自行點擊需要的房間")
+        time.sleep(int(self.textc_DelaySel.GetValue()))
         btn = driver.find_element(By.CSS_SELECTOR, 'button[data-toggle="modal"].btn.btn-primary')
         btn.click()
         time.sleep(3)        
         
         #定義變數
-        i=0
-        a=0
-        ticket=[]
-        white=[]
-        black=[]
-        cup=[]
+        i = 0
+        a = 0
+        ticket = []
+        white = []
+        black = []
+        cup = []
         player_list = []
         btn = driver.find_element(By.XPATH, '//a[text()="第二局"]')
         
         #遊戲玩家
-        p=driver.find_elements(By.CSS_SELECTOR,"[align=center]")
+        p = driver.find_elements(By.CSS_SELECTOR,"[align=center]")
         for player in p:
             player_list.append(player.text)
             
+        # 在進入房間後保存螢幕截圖
+        screenshot_filename = f"screenshot_round_{round_number}.png"
+        driver.save_screenshot(screenshot_filename)
+            
         # 打開文件以寫入模式
-        with open(path, "w", encoding="utf-8") as file:
+        with open(path, "w", encoding = "utf-8") as file:
             #定義局數、黑白球、好壞杯
             for round_number in range(1,r+1):
                 round_id = f"round_{round_number}"
-                round=driver.find_element(By.ID,round_id)#定義第幾局
-                round_tds=round.find_elements(By.TAG_NAME,"td")#派票黑白球
-                round_cups=round.find_elements(By.CLASS_NAME,"col-sm-2")#好壞杯
+                round = driver.find_element(By.ID,round_id)#定義第幾局
+                round_tds = round.find_elements(By.TAG_NAME,"td")#派票黑白球
+                round_cups = round.find_elements(By.CLASS_NAME,"col-sm-2")#好壞杯
                 
                 #紀錄派票黑白球
                 for round_td in round_tds:
@@ -178,7 +132,7 @@ class MyFrame(wx.Frame):
                         cup.append("o")
                     elif "image/bad_cup.jpg" in round_cup.get_attribute("innerHTML"):
                         cup.append("x")
-                all_cup=''.join(map(str,cup))
+                all_cup = ''.join(map(str,cup))
                 print(all_cup)
                 file.write(f"{all_cup}\n")
                 cup=[]
@@ -204,7 +158,7 @@ class MyFrame(wx.Frame):
                 time.sleep(3)
                 
             #刺客刺殺
-            print("刺客刺殺：",lake_lady[-1],sep="")
+            print("刺客刺殺：",lake_lady[-1],sep = "")
             file.write(f"刺客刺殺：{lake_lady[-1]}\n")
             
             #角色位置
@@ -293,12 +247,59 @@ class MyFrame(wx.Frame):
             print("0",player_list[6],sep=".")
             file.write(f"0.{player_list[6]}\n")
             driver.quit()
-            
-        self.btnRecord.Enable(True)            
+        self.stext_WaitSel.SetForegroundColour((0,0,255))
+        self.stext_WaitSel.SetLabel("完成紀錄")
+        
+        self.btnRecord.Enable(True)
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame(None)
-    frame.SetIcon(wx.Icon('gameboid.ico'))
+    def SettingINI(self):
+        IniBuf = {}
+        PosIndex = 0
+        path = 'Setting.ini'
+        try:
+            with open(path, 'r') as f:
+                for line in f:
+                    PosIndex+=1                                                      
+                    if line != None: 
+                        IniBuf[PosIndex] = line 
+              
+            for IndexExe in range(1,PosIndex+1):
+                if IniBuf != None:
+                    SplitExe = re.split(' |,', IniBuf[IndexExe])
+                    
+                    match SplitExe[0]:                                                                                    
+                        case 'Config':
+                            self.textc_ActualCombat.SetValue(SplitExe[1])
+                            self.textc_DelaySel.SetValue(SplitExe[2])
+                            self.tctrl_RecordName.SetValue(SplitExe[3])                     
+                        #case _:
+                            #print('Default')           
+        except FileNotFoundError:
+            APFrame.SaveAPSetting(self) 
+            
+    def SaveAPSetting(self):
+        IniBuf = {}
+        path = 'Setting.ini'
+        
+        IniBuf[0]='CONFIG,'\
+            +self.textc_ActualCombat.GetValue()+','\
+            +self.textc_DelaySel.GetValue()+','\
+            +self.tctrl_RecordName.GetValue()
+            
+        try:
+            file = open(path, "w")  
+            for x in range(1):
+                print(IniBuf[x], end="\n", file=file)
+            file.close()            
+        except IOError:
+            print("[Error] Unable to write file.")                              
+####################################################################################################
+# Run the program
+if __name__ == "__main__":               
+    app = wx.App() 
+    frame = APFrame(titleStr)
+    frame.SetIcon(wx.Icon('gameboid.ico')) 
     frame.Show()
-    app.MainLoop()
+    app.MainLoop() #start the applications
+#------------------------------------------------------------------------------------------
+#EOF
